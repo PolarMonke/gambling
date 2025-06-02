cf2f5744b0ad9312963952f124382a8da0122b0a
PolarMonke <polarmonkey000@gmail.com>
2025-06-02 18:21:45 +0300

    sign in

---
+++ src/components/SignInSuggestion.jsx
@@ -0,0 +1,18 @@
+import "../styles/SignInSuggestion.css"
+
+const SignInSuggestion = () => {
+    return (
+        <div className="sign-in-suggestion">
+            <img src="src/assets/chips.png" className="decoration chips" />
+
+            <div className="block">
+                <div>BEGIN YOUR GAMBLING CAREER NOW!</div>
+                <button>SIGN IN</button>
+            </div>
+
+            <img src="src/assets/chips.png" className="decoration chips" />
+        </div>
+    )
+}
+
+export default SignInSuggestion;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import Hero from '../components/Hero';
+import SignInSuggestion from '../components/SignInSuggestion';
 
 const Home = () => {
     return (
       <div>
         <Hero />
-        
+        <SignInSuggestion />  {/* Replace with current quests when logged in */}
       </div>
     )
 }
